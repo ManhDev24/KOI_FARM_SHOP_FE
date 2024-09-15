d7e2ffed64ed60dab7a8e0d6ca1357ce2c65e1e6
ManhDev24 <tienmanhpham2403@gmail.com>
2024-09-15 20:37:53 +0700

    create login register forgetpass and otp

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,36 @@
-import HomePage from '../components/HomePage/HomePage'
+import HomePage from "../components/HomePage/HomePage";
+import DefaultLayout from "../layouts/DefautLayout/DefaultLayout";
+import ChangePassword from "../modules/ChangePassword/ChangePassword";
+import Login from "../modules/LoginPage/Login";
+import Otp from "../modules/OTP/OTP";
+import Register from "../modules/RegisterPage/Register";
 
 export const publicRoutes = [
   {
-    path: '/',
+    path: "/",
     component: HomePage,
+    layout: DefaultLayout,
   },
-]
+  {
+    path: "/login",
+    component: Login,
+    layout: null,
+  },
+  {
+    path: "/register",
+    component: Register,
+    layout: null,
+  },
+  {
+    path: "/otp",
+    component: Otp,
+    layout: null,
+  },
+  {
+    path: "/changePassword",
+    component: ChangePassword,
+    layout: null,
+  },
+];
 
-export const privateRoutes = []
+export const privateRoutes = [];
