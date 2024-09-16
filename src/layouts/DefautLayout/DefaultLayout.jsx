5d1212d3788e12774fc20c9826686103331c4460
Hoang Tien Dat <datfullstack@gmail.com>
2024-09-16 22:25:42 +0700

    Navbar of homepage

--- src/layouts/DefautLayout/DefaultLayout.jsx
+++ src/layouts/DefautLayout/DefaultLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
+import Footer from './Footer/Footer'
 
 const DefaultLayout = ({ children }) => {
   return (
     <div>
       <div className="navbar">
         <Navbar />
+        <Footer/>
       </div>
       <div>{children}</div>
     </div>
