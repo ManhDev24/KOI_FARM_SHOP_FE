63cc986de8203e2d3a836b1dadaff23020e3bc0a
Hoang Tien Dat <datfullstack@gmail.com>
2024-09-22 21:22:22 +0700

    Fix responsive

--- src/layouts/DefautLayout/Navbar/Navbar.jsx
+++ src/layouts/DefautLayout/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { GoogleLogin } from "@react-oauth/google";
+import React from 'react';
 
-import { toast } from "react-toastify";
+
+
 import { Button, Dropdown, Menu } from 'antd';
 import './navbar.css';
 import { Link } from 'react-router-dom';
+import { getLocalStorage } from '../../../utils/LocalStorage';
 const Navbar = () => {
   // Define menu items for dropdowns
   const koiMenuItems = [
@@ -20,44 +21,29 @@ const Navbar = () => {
     { key: '3', label: 'Option 3' },
   ];
 
+  const profile = [
+    { key: '1', label: {} },
+    { key: '2', label: ' Thông tin cá nhân' },
+    { key: '3', label: 'x' },
+    { key: '4', label: 'Đăng xuất' },
+  ];
 
 
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
-  // Check if user data exists in local storage to maintain login state
-  useEffect(() => {
-    const user = localStorage.getItem("user");
-    if (user) {
-      setIsLoggedIn(true); // User is logged in
-    }
-  }, []);
+  const userlogged = getLocalStorage("User");
 
-  // Google login success handler
-  const handleGoogleLoginSuccess = (credentialResponse) => {
-    const decoded = jwt_decode(credentialResponse?.credential);
-    console.log("decoded: ", decoded);
-    localStorage.setItem("user", JSON.stringify(decoded)); // Save user data to local storage
-    setIsLoggedIn(true); // Update login state
-    toast.success("Login successful!");
-  };
 
-  // Google login error handler
-  const handleGoogleLoginError = () => {
-    toast.error("Login Failed");
-    console.log("Login Failed");
-  };
 
-  // Logout functionality (optional)
-  const handleLogout = () => {
-    localStorage.removeItem("user");
-    setIsLoggedIn(false); // Update login state
-    toast.info("Logged out successfully.");
-  };
+
 
   return (
     <>
-      <div className='Navbar grid grid-cols-6   w-full h-[150px] lg:w-full lg:h-[150px] md:h-[200px]  sm:h-[300px] sm:col-span-1'>
-        <div className='w-full h-full flex flex-col justify-center col-span-1 md:col-span-1 sm:col-span-12 items-center'>
+      {/* //navbar */}
+      <div className='Navbar md:h-[400px] lg:h-[400px] xl:grid-cols-10  grid lg:grid-cols-10  2xl:grid-cols-8   w-full h-[150px]  lg:w-full xl:h-[150px]   sm:h-[300px] sm:col-span-1'>
+
+
+        {/* //logo */}
+        <div className='w-full h-full md:col-span-12 lg:col-span-12 xl:col-span-1 flex flex-col justify-center col-span-1  sm:col-span-12 items-center'>
           <div className='logo w-[90px]  h-[90px] container ms-[50px]'>
 
             <Link to="/">
@@ -68,11 +54,10 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div className='categories  lg:col-span-3 lg:h-[150px] md:w-[300px] md:h-[200px] md:col-span-1 sm:h-[200px]'>
-          <ul>
 
-          </ul>
-          <ul className='flex flex-row items-center md:h-[200px] lg:h-[150px] md:display'>
+        {/* //cate */}
+        <div className='categories md:col-span-12 lg:col-span-12 xl:grid xl:grid-cols-1 xl:ms-[50px] xl:col-span-5 2xl:col-span-4  lg:flex lg:items-center lg:justify-center lg:h-[150px] lg:w-full md:w-full md:h-[200px] sm:h-[200px]'>
+          <ul className='flex flex-col md:flex-row items-center justify-center md:h-[200px] lg:h-[150px]'>
             <li className='me-x'>
               <Dropdown
                 menu={{ items: koiMenuItems }}
@@ -88,6 +73,7 @@ const Navbar = () => {
                 </Button>
               </Dropdown>
             </li>
+
             <li className='me-x'>
               <Dropdown
                 menu={{ items: newsMenuItems }}
@@ -103,6 +89,7 @@ const Navbar = () => {
                 </Button>
               </Dropdown>
             </li>
+
             <li className='me-x'>
               <Dropdown
                 menu={{ items: koiMenuItems }}
@@ -118,41 +105,38 @@ const Navbar = () => {
                 </Button>
               </Dropdown>
             </li>
+
             <li className='me-x'>
-              <Button type="primary" danger className='my-0 mx-auto cate-font ps-1  w-[192px]  h-[42px] box-border flex justify-center align-top pe-1'>
+              <Button type="primary" danger className='my-0 mx-auto cate-font ps-1  w-[192px] h-[42px] box-border flex justify-center align-top pe-1'>
                 <span className='w-[178px] h-[30px]'>
                   Hỗ trợ khách hàng
                 </span>
               </Button>
             </li>
-
-
-
-
           </ul>
         </div>
-        <div className=" flex lg-col items-center">
 
+        {/* //search */}
+        <div className="flex items-center justify-center md:col-span-12 lg:w-[80vw] md:w-[70vw] lg:col-span-12 xl:w-[400px] xl:col-span-1 2xl:col-span-1 max-w-full relative">
           <input
             type="search"
-            className="relative m-0 block h-[42px] flex-auto left-[40px]  border rounded-[10px] border-solid border-[#e24242]   ps-[44px] py-[0.25rem] text-base leading-[1.6]  outline-none   placeholder:text-neutral-500   focus:shadow-inset  dark:placeholder:text-[#FA4444]"
+            className="block h-[42px] flex-auto w-full border rounded-[10px] border-solid border-[#e24242] pl-[44px] py-[0.25rem] text-base leading-[1.6] outline-none placeholder:text-neutral-500 focus:shadow-inset focus:border-red-500 dark:placeholder:text-[#FA4444]"
             placeholder="Tìm Kiếm"
             aria-label="Search"
-            id="exampleFormControlInput2"
-
           />
           <span
-            className=" relative  left-[-203px] top-[0px] items-center whitespace-nowrap px-3 py-[0.25rem] text-surface dark:border-neutral-400 dark:text-[#FA4444] [&>svg]:h-5 [&>svg]:w-5"
-            id="button-addon2">
+            className="absolute left-[10px] top-1/2 transform -translate-y-1/2 text-neutral-500 dark:text-[#FA4444]"
+            id="search-icon"
+          >
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
               <path d="M18.9695 18.9695L22.7132 22.7132M22.6128 25.7455C22.3964 25.5423 22.223 25.2976 22.1029 25.0261C21.9829 24.7545 21.9186 24.4616 21.9139 24.1647C21.9092 23.8679 21.9643 23.5731 22.0757 23.2979C22.1872 23.0227 22.3528 22.7727 22.5628 22.5628C22.7727 22.3528 23.0227 22.1872 23.2979 22.0757C23.5731 21.9643 23.8679 21.9092 24.1648 21.9139C24.4616 21.9186 24.7545 21.9829 25.0261 22.1029C25.2976 22.223 25.5423 22.3964 25.7455 22.6128L30.3008 27.1681C30.5172 27.3714 30.6907 27.616 30.8107 27.8876C30.9308 28.1591 30.9951 28.452 30.9997 28.7489C31.0044 29.0458 30.9494 29.3406 30.8379 29.6157C30.7265 29.8909 30.5608 30.1409 30.3509 30.3509C30.1409 30.5608 29.8909 30.7264 29.6157 30.8379C29.3406 30.9494 29.0458 31.0044 28.7489 30.9997C28.452 30.9951 28.1591 30.9308 27.8876 30.8107C27.616 30.6907 27.3714 30.5172 27.1681 30.3008L22.6128 25.7455ZM21.9644 11.4822C21.9644 8.70216 20.8601 6.03597 18.8943 4.07017C16.9285 2.10437 14.2623 1 11.4822 1C8.70216 1 6.03597 2.10437 4.07017 4.07017C2.10437 6.03597 1 8.70216 1 11.4822C1 14.2623 2.10437 16.9285 4.07017 18.8943C6.03597 20.8601 8.70216 21.9644 11.4822 21.9644C14.2623 21.9644 16.9285 20.8601 18.8943 18.8943C20.8601 16.9285 21.9644 14.2623 21.9644 11.4822Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
-
           </span>
         </div>
 
-        <div className='w-full h-full flex justify-center items-center'>
-          <Link to="cart" className='me-10'>
+        {/* //button login,signup */}
+        <div className='w-full h-full flex justify-end items-center col-span-5 xl:col-span-3 2xl:col-span-2'>
+          <Link to="/cart" className='me-10'>
             <button className='relative  flex justify-end items-center mt-[21x]'> <div style={{ width: '100%', height: '100%', position: 'relative', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-end', display: 'inline-flex' }}>
               <div style={{ width: 42, height: 42, background: 'white', boxShadow: '4px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 5, border: '1px #EA4444 solid' }}></div>
               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="30" viewBox="0 0 31 36" fill="none" className='absolute right-[5px]' >
@@ -162,7 +146,7 @@ const Navbar = () => {
           </Link>
 
           {
-            isLoggedIn ? (
+            userlogged ? (
               <div className="w-[80.75px] h-[36.75px] relative flex-col mt-[-5px] justify-start items-start inline-flex">
 
                 <span>
