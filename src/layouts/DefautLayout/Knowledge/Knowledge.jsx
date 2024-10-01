08d1362a8c691247bfa1f60a2719ffc8ded9f249
Hoang Tien Dat <datfullstack@gmail.com>
2024-10-02 00:42:25 +0700

    Fix compare fish missed

--- src/layouts/DefautLayout/Knowledge/Knowledge.jsx
+++ src/layouts/DefautLayout/Knowledge/Knowledge.jsx
@@ -1,30 +1,24 @@
-import { Button, Col, Flex, Row } from "antd";
-import React, { useState, useEffect } from "react";
-import "./introduction.css";
-import { Link } from "react-router-dom";
-import FishApi from "../../../apis/Fish.api";
-import { useDispatch } from "react-redux";
-import { addToCart } from "../../../Redux/Slices/Cart_Slice";
+import React, { useState, useEffect } from 'react';
+import './introduction.css';
+import { Link } from 'react-router-dom';
+import FishApi from '../../../apis/Fish.api';
+import ComparisonModal from '../../../modules/Modal/ComparisonModal';
+import { Button, Col, Flex, Row } from 'antd';
 
 const Knowledge = () => {
   const [categoryResponses, setCategoryResponses] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const dispatch = useDispatch();
-  const handleAddToCart = (fish) => {
-    dispatch(
-      addToCart({
-        ...fish,
-        quantity: 1,
-      })
-    );
-  };
+  const [isModalOpen, setIsModalOpen] = useState(false); // State to manage modal visibility
+  const [selectedItems, setSelectedItems] = useState([]); // State to store the selected items for comparison
 
+  // Helper function to handle image paths
   const getImageUrl = (imgPath) => {
-    if (!imgPath) return "/img/default-banner.jpg";
-    return imgPath.startsWith("http") ? imgPath : imgPath.replace("./", "/");
+    if (!imgPath) return "/img/default-banner.jpg"; // Default image if there's no cateImg
+    return imgPath.startsWith('http') ? imgPath : imgPath.replace('./', '/');
   };
 
+  // Fetch fish data from API
   useEffect(() => {
     const fetchKoiFishData = async () => {
       try {
@@ -48,42 +42,66 @@ const Knowledge = () => {
     fetchKoiFishData();
   }, []);
 
-  if (loading) {
-    return <div>Đang tải dữ liệu...</div>;
-  }
+  // Add Koi fish to comparison list
+  const handleAddToCompare = (item) => {
+    if (selectedItems.length < 2 && !selectedItems.some((i) => i.id === item.id)) {
+      setSelectedItems([...selectedItems, item]); // Add item to the comparison list
+    } else if (selectedItems.some((i) => i.id === item.id)) {
+      alert("This item has already been added to the comparison.");
+    } else {
+      alert("You can only compare a maximum of 2 items.");
+    }
+  };
 
-  if (error) {
-    return <div>Có lỗi xảy ra: {error}</div>;
+  // Remove Koi fish from the comparison list
+  const removeItemFromCompare = (itemToRemove) => {
+    const updatedItems = selectedItems.filter((item) => item.id !== itemToRemove.id);
+    setSelectedItems(updatedItems);
+  };
+
+  // Open comparison modal
+  const handleCompare = () => {
+    if (selectedItems.length > 0) {
+      setIsModalOpen(true);
+    } else {
+      alert("Please select at least one fish to compare.");
+    }
+  };
+
+  // Close comparison modal
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  if (loading) {
+    return <div>Loading data...</div>;
   }
 
   return (
-    <>
-      {/* Tiêu đề */}
-
-      <Row justify="center">
-        <div className="w-[950px] h-[50px] bg-white text-[36px] text-[#FA4444] text-center border border-3 border-[#FA4444] my-[80px]">
-          <span
-            className="h-[38px]"
-            style={{ fontFamily: "Merriweather, serif" }}
-          >
-            KIẾN THỨC CÁ KOI
-          </span>
+    <div className='group'>
+      {/* Title */}
+      <Row justify='center'>
+        <div className='w-[950px] h-[50px] bg-white text-[36px] text-[#FA4444] text-center border border-3 border-[#FA4444] my-[80px]'>
+          <span className='h-[38px]' style={{ fontFamily: 'Merriweather, serif' }}>KIẾN THỨC CÁ KOI</span>
         </div>
       </Row>
 
+      {/* Display each category with a banner and Koi fish list */}
       {categoryResponses.map((category, index) => (
-        <div key={index}>
-          <Row justify="center" style={{ marginBottom: "80px" }}>
+        <div key={index} className='group'>
+          {/* Banner for each category */}
+          <Row justify="center" style={{ marginBottom: '80px' }}>
             <Col>
               <img
                 src={getImageUrl(category.cateImg)}
                 alt={`Banner ${index}`}
-                className="w-[2000px] h-[525px]"
+                className='w-[2000px] h-[525px]'
               />
             </Col>
           </Row>
 
-          <Flex horizontal className="justify-center ">
+          {/* Koi fish list in the category */}
+          <Flex horizontal className='justify-center'>
             <Row
               gutter={[16, 16]}
               justify="center"
@@ -92,27 +110,25 @@ const Knowledge = () => {
               {category.koiFishList && category.koiFishList.length > 0 ? (
                 category.koiFishList.map((card) => (
                   <Col
-                    key={card.id}
+                    key={card.id} // Use `id` as key
                     className="w-[250px] h-[645px] mx-10 mb-10"
                   >
+                    {/* Card content */}
                     <div className="relative w-[250px]">
-                      <div
-                        className="absolute w-[86px] 
-                                                bg-[#FFFFFF] rounded-ee-[10px] 
-                                                rounded-tl-[5px] text-center 
-                                                text-[#FA4444]"
-                      >
+                      {/* Tag */}
+                      <div className="absolute w-[86px] bg-[#FFFFFF] rounded-ee-[10px] rounded-tl-[5px] text-center text-[#FA4444]">
                         {card.status ? "Đang bán" : "Đã bán"}
                       </div>
+                      {/* Koi fish image */}
                       <div className="rounded-[10px]">
                         <img
                           src={getImageUrl(card.koiImage)}
-                          className="w-[250px] h-[354px] rounded-t-[8px] box-border"
+                          className='w-[250px] h-[354px] rounded-t-[8px] box-border'
                           alt={category.categoryName}
-                          style={{ width: "250px" }}
                         />
                       </div>
                     </div>
+                    {/* Koi fish details */}
                     <div className="flex flex-col w-[250px] h-[290px] bg-[#FFFFFF] border border-t-0 border-x-2 border-b-2 border-[#FA4444] rounded-b-[10px]">
                       <h1 className="my-0 mx-auto text-[#FA4444] font-bold text-[20px]">
                         {category.categoryName}
@@ -128,36 +144,53 @@ const Knowledge = () => {
                         </div>
                         <div className="text-center">
                           <div className="my-[10px] text-[20px] font-bold">
-                            {new Intl.NumberFormat("vi-VN", {
-                              style: "currency",
-                              currency: "VND",
-                            }).format(card.price)}
+                            {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(card.price)}
                           </div>
-
-                          <Link>
-                            <Button
-                              onClick={() => {
-                                handleAddToCart(card);
-                              }}
-                              className="w-[138px] h-[40px] text-[#FFFFFF] bg-[#FA4444] rounded-[10px]"
-                            >
+                          <Link to={`/product/${card.id}`}>
+                            <Button className='w-[138px] h-[40px] text-[#FFFFFF] bg-[#FA4444] rounded-[10px]'>
                               Đặt Mua
                             </Button>
                           </Link>
+                          <div className='relative top-[-629px] left-[130px]' onClick={() => handleAddToCompare(card)}>
+                            <Button className='!p-0 !py-1 w-[100px] !border-0 h-fit hover:!border-[#FA4444] border-[#null] hover:!text-[#FA4444] flex justify-around'>
+                              <div className='flex justify-center items-center'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" className='flex' viewBox="0 0 24 24"><g fill="none" fillRule="evenodd"><path d="M9 5a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v4h4a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-4v4a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-4H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h4z" fill="currentColor" /></g></svg>
+                                <h5 className='mx-1 my-0 !text-center'>So sánh</h5>
+                              </div>
+                            </Button>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </Col>
                 ))
               ) : (
-                <p>Không có dữ liệu cá Koi trong danh mục này.</p>
+                <p>No Koi fish data in this category.</p>
               )}
             </Row>
           </Flex>
         </div>
       ))}
-    </>
+
+      {/* Comparison Modal */}
+      <ComparisonModal
+        isOpen={isModalOpen}
+        onClose={handleModalClose}
+        selectedItems={selectedItems}
+        removeItem={removeItemFromCompare}
+      />
+
+      <Button
+        onClick={handleCompare}
+        className={`bg-[#FA4444] text-white fixed z-40 left-[100px] top-[100px] opacity-0 group-hover:opacity-100 transition-opacity duration-200 ${selectedItems.length === 0 ? 'disabled' : ''}`}
+        disabled={selectedItems.length === 0}
+      >
+        Xem So Sánh ({selectedItems.length}) Cá Koi
+      </Button>
+    </div>
   );
-};
+
+
+}
 
 export default Knowledge;
