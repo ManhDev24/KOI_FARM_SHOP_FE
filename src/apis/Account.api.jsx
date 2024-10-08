261f7a95ee1d9b789d51af4835e3b0fb4deb1db9
ManhDev24 <tienmanhpham2403@gmail.com>
2024-10-08 23:50:40 +0700

    create user management

---
+++ src/apis/Account.api.jsx
@@ -0,0 +1,46 @@
+import create from "@ant-design/icons/lib/components/IconFont";
+import fetcher from "./Fetcher";
+
+export const AccountApi = {
+  getProfile: async (email) => {
+    try {
+      const response = await fetcher.get(
+        `http://localhost:8080/koifarm/account/profile/${email}`
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response.data.message);
+    }
+  },
+  getAllAccount: async (currentPage, pageSize = 9) => {
+    try {
+      const response = await fetcher.get(
+        `http://localhost:8080/koifarm/manage/allAcount?page=${currentPage}&pageSize=${pageSize}`
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response.data.message);
+    }
+  },
+  banUser: async (id) => {
+    try {
+      const response = await fetcher.put(
+        `http://localhost:8080/koifarm/manage/updateStatus/${id}`
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response.data.message);
+    }
+  },
+  createAccount: async (data) => {
+    try {
+      const response = await fetcher.post(
+        "http://localhost:8080/koifarm/manage/createAccount",
+        data
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response.data.message);
+    }
+  },
+};
