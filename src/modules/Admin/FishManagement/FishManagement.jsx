4938787d87ed0a1d0864fa0af110db68136d2ac3
ManhDev24 <tienmanhpham2403@gmail.com>
2024-10-10 18:06:13 +0700

    fix fishManagement

--- src/modules/Admin/FishManagement/FishManagement.jsx
+++ src/modules/Admin/FishManagement/FishManagement.jsx
@@ -27,6 +27,7 @@ import {
 } from "@ant-design/icons";
 import FishApi from "../../../apis/Fish.api";
 import LoadingModal from "../../Modal/LoadingModal";
+import { Link } from "react-router-dom";
 
 const validationSchema = yup.object().shape({
   // fishName: yup.string().required("Tên cá là bắt buộc"),
@@ -51,13 +52,15 @@ const validationSchema = yup.object().shape({
   health: yup.string().required("Sức khỏe là bắt buộc"),
   temperature: yup.string().required("Nhiệt độ nước là bắt buộc"),
   ph: yup.string().required("pH nước là bắt buộc"),
-  
 });
 
 const FishManagement = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalViewOpen, setIsModalViewOpen] = useState(false);
   const [dataEdit, setDataEdit] = useState(null);
+  const [dataView, setDataView] = useState(null);
+  console.log("dataView: ", dataView);
   const [image, setImage] = useState(undefined);
 
   const queryClient = useQueryClient();
@@ -162,8 +165,8 @@ const FishManagement = () => {
             type="primary"
             icon={<EyeOutlined />}
             onClick={() => {
-              showModal();
-              setDataEdit(record);
+              showModalView();
+              setDataView(record);
             }}
           >
             View
@@ -199,7 +202,12 @@ const FishManagement = () => {
   const showModal = () => {
     setIsModalOpen(true);
   };
-
+  const showModalView = () => {
+    setIsModalViewOpen(true);
+  };
+  const cancelModalView = () => {
+    setIsModalViewOpen(false);
+  };
   const handleCancel = () => {
     setIsModalOpen(false);
     reset(); // Reset form fields
@@ -221,6 +229,7 @@ const FishManagement = () => {
       size: "",
       personality: "",
       price: "",
+      status: true,
       purebred: true,
       health: "",
       temperature: "",
@@ -262,7 +271,7 @@ const FishManagement = () => {
   }, [dataEdit, reset]);
 
   const onSubmit = (data) => {
-    console.log('data: ', data);
+    console.log("data: ", data);
   };
 
   const onEditFish = (record) => {
@@ -631,6 +640,111 @@ const FishManagement = () => {
             </Col>
           </Row>
         </form>
+      </Modal>
+
+      <Modal
+        visible={isModalViewOpen}
+        footer={null}
+        onCancel={cancelModalView}
+        className="flex justify-center items-center"
+      >
+        <Col key={dataView?.id} className="w-[250px] h-[645px] mx-10 mb-10">
+          <div className="relative w-[250px]">
+            <div
+              className="absolute border-[1px] border-[#FA4444] w-[86px] 
+                                                bg-[#FFFFFF] rounded-ee-[10px] 
+                                                rounded-tl-[5px] text-center 
+                                                text-[#FA4444]"
+            >
+              {dataView?.status === 1
+                ? "Đang bán"
+                : dataView?.status === 2
+                ? "Đã bán"
+                : null}
+            </div>
+            <div className="rounded-[10px]">
+              <img
+                src={dataView?.koiImage}
+                className="w-[250px] h-[354px] rounded-t-[8px] box-border"
+                alt={dataView?.category}
+                style={{ width: "250px" }}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col w-[250px] h-[300px] bg-[#FFFFFF] border border-t-0 border-x-2 border-b-2 border-[#FA4444] rounded-b-[10px]">
+            <h1 className="my-0 mx-auto text-[#FA4444] font-bold text-[20px]">
+              {dataView?.categoryName}
+            </h1>
+            <div className="my-[10px] mx-[10px]  ">
+              <div className="flex flex-col ">
+                <div className="h-7 text-lg font-bold flex justify-center text-[#FA4444] ">
+                  {dataView?.category} {dataView?.size} cm {dataView?.age} tuổi
+                </div>
+                <div className="h-7">Người bán: {dataView?.origin}</div>
+                <div className="h-6">
+                  Giới tính: {dataView?.gender ? "Koi Đực" : "Koi Cái"}
+                </div>
+                <div className="h-6">Tuổi: {dataView?.age}</div>
+                <div className="h-6">Kích thước: {dataView?.size} cm</div>
+                <div className="h-6">Nguồn gốc: {dataView?.origin}</div>
+                <div className="h-6">Giống: {dataView?.category}</div>
+              </div>
+              <div className="text-center">
+                <div className="my-[10px] text-[20px] font-bold">
+                  {new Intl.NumberFormat("vi-VN", {
+                    style: "currency",
+                    currency: "VND",
+                  }).format(dataView?.price)}
+                </div>
+                {dataView?.status !== 2 ? (
+                  <Link>
+                    <Button
+                      // onClick={() => {
+                      //   handleAddToCart(card);
+                      // }}
+                      className="w-[138px] h-[40px] text-[#FFFFFF] bg-[#FA4444] rounded-[10px]"
+                    >
+                      Đặt Mua
+                    </Button>
+                    <Link>
+                      <div
+                        className="absolute  top-[3px] right-[-5px] z-50" // Adjusted position: top right of the card
+                        // onClick={(e) => {
+                        //   handleAddToCompare(card);
+                        // }}
+                      >
+                        <Button
+                          onClick={(e) => {
+                            handleAddToCompare(card);
+                          }}
+                          className="!p-0 !py-1 w-[100px] !border-0 h-fit hover:!border-[#FA4444] hover:!text-[#FA4444] flex justify-around"
+                        >
+                          <div className="flex justify-center items-center">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="1em"
+                              height="1em"
+                              className="flex"
+                              viewBox="0 0 24 24"
+                            >
+                              <g fill="none" fillRule="evenodd">
+                                <path
+                                  d="M9 5a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v4h4a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-4v4a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-4H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h4z"
+                                  fill="currentColor"
+                                />
+                              </g>
+                            </svg>
+                            <h5 className="mx-1 my-0 !text-center">So sánh</h5>
+                          </div>
+                        </Button>
+                      </div>
+                    </Link>
+                  </Link>
+                ) : null}
+              </div>
+            </div>
+          </div>
+        </Col>
       </Modal>
     </div>
   );
