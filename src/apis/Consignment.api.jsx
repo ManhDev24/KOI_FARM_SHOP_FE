7c6aa1f54de7e7f10e28dd7776ec0198e1771556
ManhDev24 <tienmanhpham2403@gmail.com>
2024-10-27 12:59:49 +0700

    fixing consignment fee

--- src/apis/Consignment.api.jsx
+++ src/apis/Consignment.api.jsx
@@ -167,4 +167,15 @@ export const ConsignmentApi = {
       throw new Error(error.response.data.message);
     }
   },
+  createConsignmentFee: async (data) => {
+    try {
+      const response = await fetcher.post(
+        `http://localhost:8080/koifarm/consignmentFee/create`,
+        data
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response.data.message);
+    }
+  },
 };
