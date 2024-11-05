165ca1231945fa869b3c395885d2759b8d6bb36b
manhdev24 <quadas897@gmail.com>
2024-11-05 07:25:43 +0700

    update filter and profile

--- src/modules/Profile/Profile.jsx
+++ src/modules/Profile/Profile.jsx
@@ -7,7 +7,7 @@ import { useForm, Controller } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { AuthApi } from '../../apis/Auth.api';
-
+import url from '../../constant/constant';
 
 
 // Yup schema để xác thực dữ liệu
@@ -229,7 +229,7 @@ const Profile = () => {
       }
 
 
-      const response = await fetch(`http://localhost:8080/koifarm/account/profile/updateAvatar/${id}`, {
+      const response = await fetch(`${url}/account/profile/updateAvatar/${id}`, {
         method: 'POST',
         body: formData,
       });
